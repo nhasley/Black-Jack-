13e825f4e50d23f48b8f63dee7c53185e7b92678
nhasley <novahasley@gmail.com>
2019-07-31 23:58:42 -0500

    added scoring options in html. Added draft outline in js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//good to go
+var player = [];
+var dealer = [];
+var deck = [];
+
+let playerVal = 0;
+let dealerVal = 0;
+
+
+//needs work
+var cards = [1,2,3,4,5,6,7,8,9,10,11];
+    -querySelector for cards and attach the value?
+    -create objects?
+
+function start(){
+    if player clicks button dealRandom. if not stay null
+}
+function dealRandom() {
+    math.floor(math.random) generator
+}
+function hitMe(){
+    get random card from deck
+    .push random card from deck
+    if card makes playerVal > than 21 do bust alert. if not keep playing
+
+}
+function stay(){
+    if button is pressed then go to dealer
+
+}
+function dealer(){
+    have ai function
+    if *stay function is clicked. dealer is lower than 15 do hitMe to dealer
+}
+
+function curBet(){
+    if player wins add 5,10,20 $ 
+    if bet is done then hide button and replace with stay and hitMe buttons
+}
+
+function totAmt(){
+    if player wins then add *curBet. 
+    else player loses then take away *curBet.
+}
