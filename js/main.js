b06006269203ec7e45fc8b855d829a4682ba8653
nhasley <novahasley@gmail.com>
2019-08-01 14:48:11 -0500

    Added CSS board

--- js/main.js
+++ js/main.js
@@ -1,56 +1,72 @@
-//good to go
-var player = [];
-var dealer = [];
-// var deck = [];
-
+/*----- constants -----*/ 
+const PLAYERS = {
+    '1': 'Player 1',
+    '-1': 'Player 2',
+    'null': 'Blank' //is this needed?
+}
+/*----- app's state (variables) -----*/ 
+/*----- cached element references -----*/ 
+/*----- event listeners -----*/ 
+/*----- functions -----*/
 
 
 
 //needs work
+var board, turn, winner;
+var cards = []
+var player = [cards[i]];
+var dealer = [cards[i]];
+// var deck = [];
+
+//one way i could do this is make player 1 total positive to win 
+//and player 2 score negative to win -Math.abs turns # to neg (do this by making a function then having neg conversion within)
+
+
 let playerVal = 0;
 let dealerVal = 0;// do we need two? or just one cardVal?
 
 let cardVal? = 0;
 switch (cardVal?){
     case '2':
-        player.push('2');
+        cards.push('2');
         break;
     case '3':
-        player.push('3');
+        cards.push('3');
         break;
     case '4':
-        player.push('4');
+        cards.push('4');
         break;
     case '5':
-        player.push('5');
+        cards.push('5');
         break;
     case '6':
-        player.push('6');
+        cards.push('6');
         break;
     case '7':
-        player.push('7');
+        cards.push('7');
         break;
     case '8':
-        player.push('8');
+        cards.push('8');
         break;
     case '9':
-        player.push('9');
+        cards.push('9');
         break;
     case '10':
     case 'J':
     case 'Q':
     case 'K':
-        player.push('10');
+        cards.push('10');
         break;
     case 'A':
-        <prompt>Would you like the value of Ace to be 1 or 11?</prompt>
+        var answer = prompt("Would you like the value of Ace to be 1 or 11?")
         function(){
             if(answer === '1'){
-                player.push('1');
+                cards.push('1');
             } else {
-                player.push('11')
+                cards.push('11')
             }
         }
+        break;
 }
 
 
@@ -66,7 +82,9 @@ function start(){
 function dealRandom() {
     math.floor(math.random) generator
 }
-function hitMe(){
+function hitMe(evt){
+    var idx = parsInt(evt.target.id.raplace)
+
     get random card from deck
     .push random card from deck
     if card makes playerVal > than 21 do bust alert. if not keep playing
@@ -82,7 +100,8 @@ function dealer(){
 }
 
 function curBet(){
-    if player wins add 5,10,20 $ 
+    if player wins totAmt + curBet
+    else if player loses totAmt - curBet 
     if bet is done then hide button and replace with stay and hitMe buttons
 }
 
