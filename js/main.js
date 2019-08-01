9322f17d894c02cf68f52845cf7d4a00a40d2e21
nhasley <novahasley@gmail.com>
2019-08-01 01:22:45 -0500

    Added switch method in js as well as totAmt function. Began if statement for totAmt

--- js/main.js
+++ js/main.js
@@ -1,19 +1,67 @@
 //good to go
 var player = [];
 var dealer = [];
-var deck = [];
+// var deck = [];
 
-let playerVal = 0;
-let dealerVal = 0;
+
 
 
 //needs work
+let playerVal = 0;
+let dealerVal = 0;// do we need two? or just one cardVal?
+
+let cardVal? = 0;
+switch (cardVal?){
+    case '2':
+        player.push('2');
+        break;
+    case '3':
+        player.push('3');
+        break;
+    case '4':
+        player.push('4');
+        break;
+    case '5':
+        player.push('5');
+        break;
+    case '6':
+        player.push('6');
+        break;
+    case '7':
+        player.push('7');
+        break;
+    case '8':
+        player.push('8');
+        break;
+    case '9':
+        player.push('9');
+        break;
+    case '10':
+    case 'J':
+    case 'Q':
+    case 'K':
+        player.push('10');
+        break;
+    case 'A':
+        <prompt>Would you like the value of Ace to be 1 or 11?</prompt>
+        function(){
+            if(answer === '1'){
+                player.push('1');
+            } else {
+                player.push('11')
+            }
+        }
+}
+
+
 var cards = [1,2,3,4,5,6,7,8,9,10,11];
     -querySelector for cards and attach the value?
     -create objects?
 
 function start(){
-    if player clicks button dealRandom. if not stay null
+    if deck val = 0 then use start function
+    else if in play then change .innerhtml to a reset button. 
+        if reset button then set board to null.
 }
 function dealRandom() {
     math.floor(math.random) generator
@@ -38,7 +86,8 @@ function curBet(){
     if bet is done then hide button and replace with stay and hitMe buttons
 }
 
-function totAmt(){
-    if player wins then add *curBet. 
-    else player loses then take away *curBet.
-}
+// if player wins then add *curBet. 
+// else player loses then take away *curBet.
+let totAmt = arr => arr.reduce(player[i])
+totAmt <= 21 ? /*keep going */ : (alert("Bust!"))
+    
