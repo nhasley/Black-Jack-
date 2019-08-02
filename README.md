3752e55ad5f02d3cc60937606ee961d5f5298aa5
nhasley <novahasley@gmail.com>
2019-08-02 15:24:32 -0500

    edited html and css to fit grid

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 #Black Jack Game
-# Black-Jack-
-# Black-Jack-
+
+
+##Object:
+Beat the dealer by getting a count as close to 21 as possible, without going over 21.
+
+####
+
+
+objective
+about game
+
+play game link
+
+wireframe of game
+
+tech
+html css javascript 
+
+User persona 1
+User persona 2
+
+MVP USer stories (as a user I should be able to click the buttons and get the assigned objective
+I should see my score
+
+
+Stretch goal
+I should see my bet amount and what I have left
--- js/main.js
+++ js/main.js
@@ -1,66 +1,85 @@
-/*----- constants -----*/ 
-const PLAYERS = {
-    '1': 'Player 1',
-    '-1': 'Player 2',
-    'null': 'Blank' //is this needed?
-}
-/*----- app's state (variables) -----*/ 
-/*----- cached element references -----*/ 
-/*----- event listeners -----*/ 
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
 /*----- functions -----*/
 
 
 
-//needs work
+// //needs work
+
+let deck = [];
 var board, turn, winner;
-var cards = []
-var player = [cards[i]];
-var dealer = [cards[i]];
-// var deck = [];
+var cards = [];
+var player = [];
+var dealer = [];
+var strtTotl = 100;
+var curBet = 5;
+var bet = document.getElementsByClassName("bet1");
 
-//one way i could do this is make player 1 total positive to win 
-//and player 2 score negative to win -Math.abs turns # to neg (do this by making a function then having neg conversion within)
+
+var suits = ["s", "d", "c", "h"];
+var values = [02, 03, 04, 05, 06, 07, 08, 09, 10, "J", "Q", "K", "A"];
+var cardEl = document.querySelector(`.card${suits}${values}`);
 
 
-let playerVal = 0;
-let dealerVal = 0;// do we need two? or just one cardVal?
+function createDeck(){
+    for
+    deck.push()
+}
 
-let cardVal? = 0;
-switch (cardVal?){
-    case '2':
+//learned in class. repl(https://repl.it/@NovaHasley/hw-array-methods). It is last function.
+const dealRandom = deck.sort(function(){
+    return Math.random() - .5;
+});
+
+function createBoard(){
+    for(var i = 0; i < deck.length; i++)
+    cardEl
+}
+
+function crdAmt(){
+
+switch (values){
+    case 02:
         cards.push('2');
         break;
-    case '3':
+    case 03:
         cards.push('3');
         break;
-    case '4':
+    case 04:
         cards.push('4');
         break;
-    case '5':
+    case 05:
         cards.push('5');
         break;
-    case '6':
+    case 06:
         cards.push('6');
         break;
-    case '7':
+    case 07:
         cards.push('7');
         break;
-    case '8':
+    case 08:
         cards.push('8');
         break;
-    case '9':
+    case 09:
         cards.push('9');
         break;
-    case '10':
-    case 'J':
-    case 'Q':
-    case 'K':
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
         cards.push('10');
         break;
-    case 'A':
+    case "A":
         var answer = prompt("Would you like the value of Ace to be 1 or 11?")
         function(){
-            if(answer === '1'){
+            if(crdTotl === '1'){
                 cards.push('1');
             } else {
                 cards.push('11')
@@ -68,45 +87,55 @@ switch (cardVal?){
         }
         break;
 }
-
-
-var cards = [1,2,3,4,5,6,7,8,9,10,11];
-    -querySelector for cards and attach the value?
-    -create objects?
-
-function start(){
-    if deck val = 0 then use start function
-    else if in play then change .innerhtml to a reset button. 
-        if reset button then set board to null.
-}
-function dealRandom() {
-    math.floor(math.random) generator
-}
-function hitMe(evt){
-    var idx = parsInt(evt.target.id.raplace)
-
-    get random card from deck
-    .push random card from deck
-    if card makes playerVal > than 21 do bust alert. if not keep playing
-
-}
-function stay(){
-    if button is pressed then go to dealer
-
-}
-function dealer(){
-    have ai function
-    if *stay function is clicked. dealer is lower than 15 do hitMe to dealer
 }
 
-function curBet(){
-    if player wins totAmt + curBet
-    else if player loses totAmt - curBet 
-    if bet is done then hide button and replace with stay and hitMe buttons
-}
 
-// if player wins then add *curBet. 
-// else player loses then take away *curBet.
-let totAmt = arr => arr.reduce(player[i])
-totAmt <= 21 ? /*keep going */ : (alert("Bust!"))
-    
+var playerVal = player.reduce(function(accumuator, currentVal){
+    return accumuator + currentVal;
+},0);
+var dealerVal = dealer.reduce(function(accumuator, currentVal){
+    return accumuator + currentVal;
+},0);
+
+// var deck = [];
+
+//one way i could do this is make player 1 total positive to win
+//and player 2 score negative to win -Math.abs turns # to neg (do this by making a function then having neg conversion within)
+
+
+//     -querySelector for cards and attach the value?
+//     -create objects?
+
+// function start(pBet){
+//     cards = [];
+//     player = [];
+//     dealer = [];
+// }
+
+// function hitMe(evt){
+//     var idx = parsInt(evt.target.id.raplace)
+
+//     get random card from deck
+//     .push random card from deck
+//     if card makes playerVal > than 21 do bust alert. if not keep playing
+
+// }
+// function stay(){
+//     if button is pressed then go to dealer
+
+// }
+// function dealer(){
+//     have ai function
+//     if *stay function is clicked. dealer is lower than 15 do hitMe to dealer
+// }
+
+// function curBet(){
+//     if player wins totAmt + curBet
+//     else if player loses totAmt - curBet
+//     if bet is done then hide button and replace with stay and hitMe buttons
+// }
+
+// // if player wins then add *curBet.
+// // else player loses then take away *curBet.
+// let totAmt = arr => arr.reduce(player[i])
+// totAmt <= 21 ? /*keep going */ : (alert("Bust!"))
