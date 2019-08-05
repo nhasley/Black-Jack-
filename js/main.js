00c937921e4536a8b8708f83190c8814eff40f74
nhasley <novahasley@gmail.com>
2019-08-05 14:36:12 -0500

    added deck render. Showing on page, but showing whole deck instead of one card.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 //defining elements
-var money = 100;
 var dCards = [];
 var pCards = [];
-var dScore = 0;
-var pScore = 0;
+var dTotal = 0;
+var pTotal = 0;
 var deck = [];
-var bet = 10;
-var btnBet = document.getElementsByClassName("btnBet");
+
+// var money = 100;
+// var bet = 10;
+// var btnBet = document.getElementsByClassName("btnBet");
 
 var suits = ["s", "c", "d", "h"];
 var ranks = [
@@ -38,69 +39,7 @@ function status() {
 document.getElementById("hit").addEventListener("click", crdHit);
 function crdHit() {
   pCards.push(shuffledDeck.pop());
-  cardTotal();
-}
-
-function pullScore(array){
-  let score = 0;
-  let ace = false;
-  for(let i = 0; i < array.length; i++){ //use reduce instead?
-    let card = array[i];
-    score += cardVal(card);
-    if(card.value == "A"){
-      ace = true;
-    }
-    if(ace && score + 10 <= 21){ //if there's an ace and score+10 is less than 21
-      return score + 10;
-    }
-  }
-  return score;
-}
-
-function newScore() {
-  dScore = pullScore(dCards);
-  pScore = pullScore(pCards);
-}
-
-function cardVal(card){
-switch (values){
-  case "A":
-    return 1;
-    break;
-  case 02:
-    return 2;
-    break;
-  case 03:
-    return 3;
-    break;
-  case 04:
-    return 4;
-    break;
-  case 05:
-    return 5;
-    break;
-  case 06:
-    return 6;
-    break;
-  case 07:
-    return 7;
-    break;
-  case 08:
-    return 8;
-    break;
-  case 09:
-    return 9;
-    break;
-  case 10:
-  case "J":
-  case "Q":
-  case "K":
-    return 10;
-    break;
-}
-
-function cardTotal() {
-  if(dScore)
+  cTotal();
 }
 
 document.getElementById("stay").addEventListener("click", stayFunc);
@@ -134,7 +73,6 @@ function buildMasterDeck() {
   });
   return deck;
 }
-
 var masterDeck = buildMasterDeck();
 var shuffledDeck;
 renderShuffledDeck();
@@ -147,6 +85,83 @@ function renderShuffledDeck() {
   }
   // renderDeckInContainer(shuffledDeck, shuffledContainer);
 }
+var pContainer = document.getElementById("p-cards");
+function renderCards(deck, container) {
+  container.innerHTML = "";
+  var cardsHtml = shuffledDeck.reduce(function(html, card) {
+    return html + `<div class="card ${card.face}"></div>`;
+  }, "");
+  container.innerHTML = cardsHtml;
+}
+var card = renderCards(shuffledDeck.pop(), pContainer);
+
+// var pContainer = document.getElementById('p-cards');
+// var cardEl = document.createElement('div');
+// cardEl.classList.add("card ${card.face}")
+
+// // -------------
+
+// function cardVal(card){
+//   switch (values){
+//     case "A":
+//       return 1;
+//       break;
+//     case 02:
+//       return 2;
+//       break;
+//     case 03:
+//       return 3;
+//       break;
+//     case 04:
+//       return 4;
+//       break;
+//     case 05:
+//       return 5;
+//       break;
+//     case 06:
+//       return 6;
+//       break;
+//     case 07:
+//       return 7;
+//       break;
+//     case 08:
+//       return 8;
+//       break;
+//     case 09:
+//       return 9;
+//       break;
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//       return 10;
+//       break;
+//   }
+
+// function pullScore(array){
+//   let score = 0;
+//   let ace = false;
+//   for(let i = 0; i < array.length; i++){ //use reduce instead?
+//     let card = array[i];
+//     score += cardVal(card);
+//     if(card.value == "A"){
+//       ace = true;
+//     }
+//     if(ace && score + 10 <= 21){ //if there's an ace and score+10 is less than 21
+//       return score + 10;
+//     }
+//   }
+//   return score;
+// }
+
+// function newScore() {
+//   dTotal = pullScore(dCards);
+//   pTotal = pullScore(pCards);
+// }
+
+// // function cTotal() {}
+
+// // -----------------------
 
 // // function renderDeckInContainer(deck, container) {
 // //   container.innerHTML = "";
@@ -181,7 +196,7 @@ function renderShuffledDeck() {
 //     return Math.random() - .5;
 // });
 
-// 
+//
 // }
 // }
 
@@ -193,20 +208,11 @@ function renderShuffledDeck() {
 // },0);
 // ------------------
 
-// var deck = [];
-
 //one way i could do this is make player 1 total positive to win
 //and player 2 score negative to win -Math.abs turns # to neg (do this by making a function then having neg conversion within)
 
 //     -querySelector for cards and attach the value?
 //     -create objects?
-
-// function hitMe(evt){
-//     var idx = parsInt(evt.target.id.raplace)
-
-//     get random card from deck
-//     .push random card from deck
-//     if card makes playerVal > than 21 do bust alert. if not keep playing
 
 // }
 // function stay(){
